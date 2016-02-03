3fe60a62577240e26ba40ee0f558e52eebc10916
sezj <sarahzachrich@gmail.com>
2016-02-03 15:47:41 -0500

    changes to Events Manager event submission and edit forms
    remove map from submission form
    adjust event and form formatting
    add "host" custom attribute and conditional placeholder

--- plugins/events-manager/forms/event-editor.php
+++ plugins/events-manager/forms/event-editor.php
@@ -78,7 +78,7 @@ if( !empty($_REQUEST['success']) ){
 					<p><em><?php esc_html_e( 'Details about the event.', 'dbem' )?> <?php esc_html_e( 'HTML allowed.', 'dbem' )?></em></p>
 				<?php endif; ?>
 			</div>
-			<div class="event-extra-details">
+			<div class="event-extra-details clearfix">
 				<?php if(get_option('dbem_attributes_enabled')) { em_locate_template('forms/event/attributes-public.php',true); }  ?>
 				<?php if(get_option('dbem_categories_enabled')) { em_locate_template('forms/event/categories-public.php',true); }  ?>
 			</div>
